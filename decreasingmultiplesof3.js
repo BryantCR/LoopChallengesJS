fadb13940f6616ee5dfa0e2a0a719ac1bf3cca2b
BryantCR <bryancvargas20@gmail.com>
2021-09-10 15:42:10 -0600

    all working as expected

---
+++ decreasingmultiplesof3.js
@@ -0,0 +1,16 @@
+for(let i = 100; i>=0; i--){
+    if(i % 3 == 0){
+        console.log(i)
+    }
+    else{
+        
+    }
+}
+
+let i = 100;
+while(i >= 0) {
+    if(i % 3 == 0){
+        console.log("Divisible by 3", i)
+    }
+    i--
+}
